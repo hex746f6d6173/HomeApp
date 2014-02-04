a755c28536c5b6cfa2c1a95b33730af847282299
Tomas Harkema <info@tomasharkema.nl>
2014-02-04 14:03:46 +0100

    Don’t let him hang there

--- node/server.js
+++ node/server.js
@@ -6,7 +6,8 @@ app.use(express.static(__dirname + '/public'));
 var flipSwitch = function(req, res){
 
 	console.log(req.params);
-
+	res.send(JSON.stringify({"success":true}));
+	res.end();
 }
 
 app.get('/switch/:brand/:code/:switch/:switchTo/', flipSwitch, function(req, res){
