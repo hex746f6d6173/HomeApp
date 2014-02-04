d7990c1b86563e77150e6d5fdd92e895c0331563
Tomas Harkema <info@tomasharkema.nl>
2014-02-04 12:26:43 +0100

    Show command

--- switch.php
+++ switch.php
@@ -4,5 +4,6 @@ $d=$_GET["d"];
 $u=$_GET["u"];
 $s=$_GET["s"];
 $output["status"]=exec("cd /var/www/home/rc/ex/lights && sudo ./$b $d $u $s");
+$output['q'] = "cd /var/www/home/rc/ex/lights && sudo ./$b $d $u $s";
 echo json_encode($output);
 ?>
