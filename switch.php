a8efc1444d88a0ccee38a67806fddb8ca1042cfc
Tomas Harkema <info@tomasharkema.nl>
2014-02-04 12:31:11 +0100

    Qui?

--- switch.php
+++ switch.php
@@ -5,5 +5,6 @@ $u=$_GET["u"];
 $s=$_GET["s"];
 $output["status"]=exec("cd /var/www/home/rc/ex/lights && sudo ./$b $d $u $s");
 $output['q'] = "cd /var/www/home/rc/ex/lights && sudo ./$b $d $u $s";
+$output['qui'] = exec("whoami");
 echo json_encode($output);
 ?>
