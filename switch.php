5ae41786a35d876b351e3e1512c37f236464d9fc
Tomas Harkema <info@tomasharkema.nl>
2014-02-04 12:23:15 +0100

    Fix package location

--- switch.php
+++ switch.php
@@ -3,6 +3,6 @@ $b=$_GET["b"];
 $d=$_GET["d"];
 $u=$_GET["u"];
 $s=$_GET["s"];
-$output["status"]=exec("cd /home/pi/rc/ex/lights && sudo ./$b $d $u $s");
+$output["status"]=exec("cd /var/www/home/rc/ex/lights && sudo ./$b $d $u $s");
 echo json_encode($output);
 ?>
