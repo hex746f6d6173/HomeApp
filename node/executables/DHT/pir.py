1a93494d214677c64b42aba28c685ae04bf7bba6
Tomas Harkema <info@tomasharkema.nl>
2014-02-15 13:54:08 +0100

    PIR do request

--- node/executables/DHT/pir.py
+++ node/executables/DHT/pir.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import requests
 import time
 import RPi.GPIO as io
 io.setmode(io.BCM)
@@ -11,4 +11,7 @@ io.setup(pir_pin, io.IN)         # activate input
 while True:
     if io.input(pir_pin):
         print("PIR ALARM!")
+        r = requests.get("http://home.tomasharkema.nl/pir/1/1/")
+    else:
+    	r = requests.get("http://home.tomasharkema.nl/pir/1/0/")
     time.sleep(0.5)
