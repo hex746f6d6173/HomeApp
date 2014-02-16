1ad3115290885286d1b83eb7788873a1b492098d
Tomas Harkema <info@tomasharkema.nl>
2014-02-17 00:40:06 +0100

    FIX FOR GOOD!

--- node/server.js
+++ node/server.js
@@ -319,29 +319,19 @@ app.get('/pir/:a/:b', function(req, res) {
         timeSwitch = new Date().getTime();
 
         if (config.PIR.onDetectYes !== undefined) {
-            log.add(config.PIR.onDetectYes);
             config.PIR.onDetectYes.forEach(function(item) {
                 var t = new Date().getHours();
                 var check = true;
-                log.add(item);
                 if (item.time === true) {
                     check = false;
                     item.between.forEach(function(betweenDiff) {
-                        log.add(t + " > " + betweenDiff[0] + " && " + t + " < " + betweenDiff[1]);
-                        if (t > betweenDiff[0] && t < betweenDiff[1])
+                        if (t >= betweenDiff[0] && t <= betweenDiff[1])
                             check = true;
                     });
                 }
-                log.add("AUTO COMMAND CHECK" + check);
                 if (check) {
 
-                    log.add("CHECK: " + item.type);
                     if (item.type === "switch" && triggerArm === 1) {
-
-
-                        log.add("SWICH");
-
-                        log.add("ITEM, FLIP", item);
 
                         log.add("AUTO COMMAND DELAY" + item.delay);
 
@@ -385,16 +375,13 @@ app.get('/pir/:a/:b', function(req, res) {
                 if (item.time === true) {
                     check = false;
                     item.between.forEach(function(betweenDiff) {
-                        if (t > betweenDiff[0] && t < betweenDiff[1])
+                        if (t >= betweenDiff[0] && t <= betweenDiff[1])
                             check = true;
                     });
                 }
-                log.add("AUTO COMMAND CHECK" + check);
                 if (check) {
 
                     if (item.type == "switch" && triggerArm === 1) {
-
-                        console.log("ITEM, FLIP", item);
 
                         log.add("AUTO COMMAND DELAY" + item.delay);
 
