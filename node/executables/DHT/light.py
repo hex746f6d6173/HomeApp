88b3ecb479495053cb0c67bf2e00a3adfdec0252
Tomas Harkema <info@tomasharkema.nl>
2014-02-17 23:17:39 +0100

    Fix light

--- node/executables/DHT/light.py
+++ node/executables/DHT/light.py
@@ -3,12 +3,21 @@ import serial
 import requests
 import time
 
+state = 0;
+
 ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
+
+		time.sleep(10)
+
         temp =  ser.readline()
         temp = temp.rstrip()
 
-        print("http://home.tomasharkema.nl/light/"+temp+"/")
-        r = requests.get("http://home.tomasharkema.nl/light/"+temp+"/")
+        if(temp != state):
+        	state = temp;
 
-        time.sleep(10)
+	        print("http://home.tomasharkema.nl/light/"+temp+"/")
+	        
+	        r = requests.get("http://home.tomasharkema.nl/light/"+temp+"/")
+
+	        
