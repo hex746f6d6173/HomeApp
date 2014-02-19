96b7c4611f8d1d4e488e1142382cbb51681265d2
Tomas Harkema <info@tomasharkema.nl>
2014-02-19 17:17:07 +0100

    15 min after last pir 0

--- node/executables/DHT/pir.py
+++ node/executables/DHT/pir.py
@@ -10,16 +10,19 @@ io.setup(pir_pin, io.IN)         # activate input
 
 state = 0;
 
+previousTime = 0;
+
 while True:
     if io.input(pir_pin):
         if (state == 0) :
             state = 1
             print("PIR ALARM!")
+            previousTime = int(round(time.time() * 1000))
             r = requests.get("http://home.tomasharkema.nl/pir/1/1/")
     else:
-        if (state == 1) :
-            state = 0
-            print("PIR NO ALARM!")
+        print("PIR NO ALARM!")
+        now = int(round(time.time() * 1000));
+        if ((previousTime +  (1000 * 60 * 15)) <  now):
             r = requests.get("http://home.tomasharkema.nl/pir/1/0/")
                 
     time.sleep(0.5)
