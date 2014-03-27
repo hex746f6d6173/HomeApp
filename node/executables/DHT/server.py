074e351de67eb52ef39934ecdf4c4b1d23fcd0ae
Tomas Harkema <info@tomasharkema.nl>
2014-03-27 23:49:56 +0100

    doh

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -17,7 +17,7 @@ lumen = 0
 
 def updateUI():
     lcd.lcd_display_string("HOME APP     C:I T:0", 1)
-    lcd.lcd_display_string(temp + "oC / "+lumen+"Lux", 2)
+    lcd.lcd_display_string(tempratuur + "oC / "+lumen+"Lux", 2)
     lcd.lcd_display_string("", 3)
     lcd.lcd_display_string("Status: All fine!", 4)
 
