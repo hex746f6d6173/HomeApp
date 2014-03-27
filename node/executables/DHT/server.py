971bf3545fa442c3d2a71561e80911b45beb40df
Tomas Harkema <info@tomasharkema.nl>
2014-03-27 23:03:05 +0100

    fix import

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python
+from lcd import lcddriver
 
-import time
+from time import *
 
-from lcd import lcd
+lcd = lcddriver.lcd()
 
-from i2clibraries import i2c_lcd_smbus
-
-lcd1 = lcddriver.lcd()
-
-lcd2 = i2c_lcd_smbus.i2c_lcd(0x3f,1, 2, 1, 0, 4, 5, 6, 7, 3)
-lcd2.command(lcd2.CMD_Display_Control | lcd2.OPT_Enable_Display)
-lcd2.backLightOn()
+lcd.lcd_display_string("11111111111111111111222222222222222222223333333333333333333344444444444444444444", 1)
