a04fee20036ca51479bdb0a4a152f472e708acf2
Tomas Harkema <info@tomasharkema.nl>
2014-03-27 23:23:36 +0100

    Wait for callback

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -8,13 +8,13 @@ from socketIO_client import SocketIO
 def on_aaa_response(*args):
     print 'on_aaa_response', args
 
+    lcd = lcddriver.lcd()
+
+	lcd.lcd_display_string("HOME APP     C:I T:0", 1)
+	lcd.lcd_display_string("16oC / 16Lux", 2)
+	lcd.lcd_display_string("", 3)
+	lcd.lcd_display_string("Status: All fine!", 4)
+
 socketIO = SocketIO('home.tomasharkema.nl', 80)
-socketIO.wait()
+//socketIO.wait()
 socketIO.on('temp', on_aaa_response)
-
-lcd = lcddriver.lcd()
-
-lcd.lcd_display_string("HOME APP     C:I T:0", 1)
-lcd.lcd_display_string("16oC / 16Lux", 2)
-lcd.lcd_display_string("", 3)
-lcd.lcd_display_string("Status: All fine!", 4)
