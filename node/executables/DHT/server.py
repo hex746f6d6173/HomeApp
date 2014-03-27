f749cb3d0c0a4b0e32d0c0c9c80602012a8cc566
Tomas Harkema <info@tomasharkema.nl>
2014-03-27 23:24:37 +0100

    Fix identation

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -9,11 +9,10 @@ def on_aaa_response(*args):
     print 'on_aaa_response', args
 
     lcd = lcddriver.lcd()
-
-	lcd.lcd_display_string("HOME APP     C:I T:0", 1)
-	lcd.lcd_display_string("16oC / 16Lux", 2)
-	lcd.lcd_display_string("", 3)
-	lcd.lcd_display_string("Status: All fine!", 4)
+    lcd.lcd_display_string("HOME APP     C:I T:0", 1)
+    lcd.lcd_display_string("16oC / 16Lux", 2)
+    lcd.lcd_display_string("", 3)
+    lcd.lcd_display_string("Status: All fine!", 4)
 
 socketIO = SocketIO('home.tomasharkema.nl', 80)
 //socketIO.wait()
