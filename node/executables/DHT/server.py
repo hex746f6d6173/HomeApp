d4abcc454e54b240a5a00d71166a64c53fa6ea1c
root <info@tomasharkema.nl>
2014-03-27 23:04:40 +0000

    All together

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -4,7 +4,10 @@
 #logging.basicConfig(level=logging.DEBUG)
 
 import lcddriver
-from time import *
+#from time import *
+
+import time
+from time import gmtime, strftime
 
 from socketIO_client import SocketIO
 
@@ -12,23 +15,28 @@ lcd = lcddriver.lcd()
 
 print 'HALLO'
 
-tempratuur = ""
-lumen = ""
+tempratuur = "0"
+lumen = "0"
 
 def updateUI():
-    lcd.lcd_display_string("HOME APP     C:I T:0", 1)
+    global tempratuur
+    lcd.lcd_display_string("HOME APP   "+strftime("%H:%M:%S", gmtime()), 1)
     lcd.lcd_display_string(tempratuur + "oC / "+lumen+"Lux", 2)
     lcd.lcd_display_string("", 3)
     lcd.lcd_display_string("Status: All fine!", 4)
 
 def temp(*args):
+    global tempratuur
     print 'on_aaa_response', args, args[0], str(args[0])
     tempratuur = str(args[0]);
     updateUI()
-
 
 with SocketIO('home.tomasharkema.nl', 80) as socketIO:
     socketIO.on('temp', temp)
     socketIO.emit('me', 'python')
     socketIO.wait_for_callbacks(seconds=1000)
     socketIO.wait()
+
+while True:
+    updateUI()
+    time.sleep(1)
