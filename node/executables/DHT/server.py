451542d9a12e925a6ba63c7adc1ec3c87f1d4350
Tomas Harkema <info@tomasharkema.nl>
2014-03-27 23:47:52 +0100

    Eerste stap modulair

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -7,18 +7,28 @@ import lcddriver
 from time import *
 
 from socketIO_client import SocketIO
-print 'HALLO'
-def on_aaa_response(*args):
-    print 'on_aaa_response', args
 
-    lcd = lcddriver.lcd()
-    lcd.lcd_display_string("HOME APP     C:I T:0", 1)
-    lcd.lcd_display_string(str(args[0]) + "oC / 16Lux", 2)
+lcd = lcddriver.lcd()
+
+print 'HALLO'
+
+temp = 0
+lumen = 0
+
+def updateUI():
+	lcd.lcd_display_string("HOME APP     C:I T:0", 1)
+    lcd.lcd_display_string(temp + "oC / "+lumen+"Lux", 2)
     lcd.lcd_display_string("", 3)
     lcd.lcd_display_string("Status: All fine!", 4)
 
+def temp(*args):
+    print 'on_aaa_response', args
+    temp = str(args[0]);
+    updateUI()
+
+
 with SocketIO('home.tomasharkema.nl', 80) as socketIO:
-    socketIO.on('temp', on_aaa_response)
+    socketIO.on('temp', temp)
     socketIO.emit('me', 'python')
     socketIO.wait_for_callbacks(seconds=1000)
     socketIO.wait()
