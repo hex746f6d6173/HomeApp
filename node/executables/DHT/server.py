f866c61ff4f803d1c47a4e49d8804843ceb26ea3
Tomas Harkema <info@tomasharkema.nl>
2014-03-28 18:37:20 +0100

    Define zone

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -22,6 +22,18 @@ import datetime
 
 from datetime import tzinfo,timedelta
 
+class Zone(tzinfo):
+	def __init__(self,offset,isdst,name):
+		self.offset = offset
+		self.isdst = isdst
+		self.name = name
+	def utcoffset(self, dt):
+		return timedelta(hours=self.offset) + self.dst(dt)
+	def dst(self, dt):
+			return timedelta(hours=1) if self.isdst else timedelta(0)
+	def tzname(self,dt):
+		 return self.name
+
 lcd = lcddriver.lcd()
 
 print 'HALLO'
