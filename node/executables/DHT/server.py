97b6e71871b68051d538b7bdcec5c3a1637d1941
Tomas Harkema <info@tomasharkema.nl>
2014-03-28 15:24:47 +0100

    Start bed thread

--- node/executables/DHT/server.py
+++ node/executables/DHT/server.py
@@ -259,14 +259,20 @@ try:
 	thread2 = timeThread()
 	thread3 = pirThread()
 	thread4 = tempThread()
+	thread5 = bedThread()
+
 	thread3.daemon=True
 	thread2.daemon=True
 	thread1.daemon=True
 	thread4.daemon=True
+	thread5.daemon=True
+
 	thread1.start()
 	thread2.start()
 	thread3.start()
 	thread4.start()
+	thread5.start()
+
 	while True: time.sleep(100)
 except (KeyboardInterrupt, SystemExit):
 	print '\n! Received keyboard interrupt, quitting threads.\n'
